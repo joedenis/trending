6b4b4a08c491021fed96171b8a9bec87a64ee967
Joe <joseph.denis@gmail.com>
2019-01-16 11:06:37 +0000

    US Sector ETFS

---
+++ sectors_data.py
@@ -0,0 +1,12 @@
+import data_build
+
+sectors = ["XLU", "XLV", "XLY", "XLK", "XLP", "XLI", "XLF", "XLB", "XLE"]
+
+# ticker = '^GSPC'
+source = 'yahoo'
+date_start = '1999-01-01'
+# symbol = 'SPX'
+date_end = '2019-01-11'
+
+for sector in sectors:
+    data_build.main(sector, source, date_start, date_end, sector)
