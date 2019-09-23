7469a6a13378a20a0329e849b7dcc96f8d3ccefa
joe <joseph.denis@gmail.com>
2019-09-23 16:06:54 +0100

    all weather portfolio added

--- trending/accelerating_momo_data.py
+++ trending/accelerating_momo_data.py
@@ -4,6 +4,7 @@ from trending import data_build
 def main():
     world = ["SPY", "VINEX", "VUSTX"]
     sectors = ["XLB", "XLE", "XLF", "XLI", "XLK", "XLP", "XLU", "XLV", "XLY"]
+    all_weather = ["SPY", "IEF", "TLT", "GLD", "DBC"]
 
     # ticker = '^GSPC'
     source = 'yahoo'
@@ -11,7 +12,7 @@ def main():
     # symbol = 'SPX'
     date_end = '2019-08-11'
 
-    for instrument in sectors:
+    for instrument in all_weather:
         data_build.main(instrument, source, date_start, date_end, instrument)
 
 if __name__ == "__main__":
