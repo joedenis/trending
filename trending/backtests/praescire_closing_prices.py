bf11c4a8cc4d8397f5ebd73f4ebeaf0e56c244fa
joe <joseph.denis@gmail.com>
2019-10-03 20:10:18 +0100

    changed tearsheet for max daily min daily.  Bit of  asqueeze maybe revert

--- trending/backtests/praescire_closing_prices.py
+++ trending/backtests/praescire_closing_prices.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def praescire_closing_prices(
 		path="/home/joe/Dropbox/praescire_database/", filename='Trades database 2019.xlsm',
-		sheet_number=2):
+		sheet_number=2, save_name="PRAESCIRE19.csv"):
 	"""
 	to create open high low Close adj close volume prices of the praescire price in order to run the tearsheet from
 	the backtester
@@ -22,8 +22,8 @@ def praescire_closing_prices(
 	closing_prices['Volume'] = 100000000
 
 	closing_prices = closing_prices[['Date', 'High', 'Low', 'Open', 'Close', 'Volume', 'Adj Close']]
-	filename = "PRAESCIRE19.csv"
-	closing_prices.to_csv('~/PycharmProjects/trending/trending/data/'+filename, index=False)
+
+	closing_prices.to_csv('~/PycharmProjects/trending/trending/data/'+ save_name, index=False)
 
 	print(closing_prices)
 
